0978238cc0722736ab576eb4480f6898d5c31661
itsLoreena <frida.niklasson88@gmail.com>
2017-12-14 14:26:48 +0100

    Lite ändringar i User

--- Data-LogicLayer/Models/User.cs
+++ Data-LogicLayer/Models/User.cs
@@ -2,6 +2,7 @@
 using Data_LogicLayer.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -11,7 +12,10 @@ namespace Data_LogicLayer.Models
     public class User:IEntity
     {
         public int Id { get; set; }
+
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
 
         public virtual Profile Profile { get; set; }
