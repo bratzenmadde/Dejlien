afbbdc2d1fa8b8617e2842dfdd92c1392bee363f
Linn Vretlund <32539427+Lvretlund@users.noreply.github.com>
2017-12-20 14:42:28 +0100

    hej

--- DejlienApp/Controllers/AccountController.cs
+++ DejlienApp/Controllers/AccountController.cs
@@ -51,7 +51,7 @@ namespace DejlienApp.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("ModifyProfile", "Account");
+                    return RedirectToAction("Login", "Account");
                 }
                 //AddErrors(result);
             }
--- DejlienApp/Models/Profile.cs
+++ DejlienApp/Models/Profile.cs
@@ -12,7 +12,6 @@ namespace DejlienApp.Models
     
     public class Profile : IEntity
     {
-        
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
