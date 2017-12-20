a40156210f861081348205d38b381f501a5a4da9
Matilda Olsson <matilda.la.olsson@live.se>
2017-12-20 11:01:07 +0100

    kommentar om att göra alla Lists i model till icollection

--- DejlienApp/Models/Profile.cs
+++ DejlienApp/Models/Profile.cs
@@ -30,7 +30,7 @@ namespace DejlienApp.Models
         public string Description { get; set; }
 
         public virtual UserAccount UserAccount { get; set; }
-        public List<Interest> Interests { get; set; }
+        public List<Interest> Interests { get; set; }//gör alla List<> till ICollection<>
         public List<Contact> Contacts { get; set; }
         public List<Post> Posts { get; set; }
     }
