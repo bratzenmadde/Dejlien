69aa7e821d2c8d032e4a0c8a9cee561fa4bcfe3a
Matilda Olsson <matilda.la.olsson@live.se>
2017-12-21 14:23:56 +0100

    Mucho

--- DejlienApp/Controllers/AccountController.cs
+++ DejlienApp/Controllers/AccountController.cs
@@ -112,6 +112,7 @@ namespace DejlienApp.Controllers
         {
             if (!ModelState.IsValid)
             {
+
                 //var currentUserId = await accountUserManager.FindByIdAsync(User.Identity.GetUserId());
                 //using (var db = new DataContext())
                 //{
@@ -123,7 +124,7 @@ namespace DejlienApp.Controllers
                 //    else
                 //        return RedirectToAction("ModifyProfile");
                 //}
-                return View(model);
+                return View("ModifyProfile");
             }
 
             var result = await applicationSignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: true);
@@ -143,6 +144,15 @@ namespace DejlienApp.Controllers
                 default:
                     ModelState.AddModelError("", "Invalid login attempt.");
                     return View(model);
+            }
+        }
+
+        public ActionResult PersonalUserSite()
+        {
+            using (var db = new DataContext())
+            {
+                var users = db.Profiles.ToList();
+                return View(users);
             }
         }
 
