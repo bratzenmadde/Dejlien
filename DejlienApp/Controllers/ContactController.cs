7362a89ec2750b7a3ad56c941db691e1834241db
Linn Vretlund <32539427+Lvretlund@users.noreply.github.com>
2018-01-02 16:26:11 +0100

    tagit bort home, profile i navbar, lagt till contacts men pathsen fungerar inte så bra

--- DejlienApp/Controllers/ContactController.cs
+++ DejlienApp/Controllers/ContactController.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DejlienApp.Repositories;
 
 namespace DejlienApp.Controllers
 {
     public class ContactController : Controller
     {
         // GET: Contact
-        public ActionResult Index()
+        public ActionResult ListOfContacts()
         {
-            return View();
+            using (var db = new DataContext())
+            {
+                var contacts = db.Profiles.ToList();
+                return View(contacts);
+            }
+
         }
     }
 }
