387da5ca2c019de4cfd0f896529682727e6702f0
Matilda Olsson <matilda.la.olsson@live.se>
2018-01-04 17:23:17 +0100

    zrg

--- DejlienApp/Controllers/PostController.cs
+++ DejlienApp/Controllers/PostController.cs
@@ -18,54 +18,9 @@ namespace DejlienApp.Controllers
             using (var db = new DataContext())
             {
                 var posts = db.Posts.Where(x => x.Receiver.Id == id).ToList();
-                return View(new PostIndexViewModel { Id = id, Posts = posts });
+                return View(new Profile { Id = id, Posts = posts });
             }
         }
-
-        [HttpPost]
-        public ActionResult Create(Post post, int id, HttpPostedFileBase upload)
-        {
-            var userId = User.Identity.GetUserId();
-
-            using (var db = new DataContext())
-            {
-                var currentUser = db.Users.Single(u => u.Id.ToString() == userId);
-                var userProfile = currentUser.Profile;
-
-
-                post.Author = userProfile;
-
-                var toUser = db.Profiles.Single(x => x.Id == id);
-                post.Receiver = toUser;
-
-                db.Posts.Add(post);
-
-                //if (upload != null && upload.ContentLength > 0)
-                //{
-                //    post.Filename = upload.FileName;
-                //    post.ContentType = upload.ContentType;
-
-                //    using (var reader = new BinaryReader(upload.InputStream))
-                //    {
-                //        post.File = reader.ReadBytes(upload.ContentLength);
-                //    }
-                //}
-
-                db.SaveChanges();
-
-                return RedirectToAction("Index", new { id = id });
-            }
-        }
-
-        //public ActionResult Image(int id)
-        //{
-        //    using (var db = new DataContext())
-        //    {
-        //        var post = db.Posts.Single(x => x.Id == id);
-
-        //        return File(post.File, post.ContentType);
-        //    }
-        //}
     }
 
     public class PostIndexViewModel
