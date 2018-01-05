5089e2adf9054adf29f7b200a302d01302347620
itsLoreena <frida.niklasson88@gmail.com>
2018-01-05 10:52:35 +0100

    Kan adda friend

--- DejlienApp/Controllers/ContactController.cs
+++ DejlienApp/Controllers/ContactController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using DejlienApp.Repositories;
 using Microsoft.AspNet.Identity;
+using DejlienApp.Models;
+using System.Data.Entity;
 
 namespace DejlienApp.Controllers
 {
@@ -25,7 +27,39 @@ namespace DejlienApp.Controllers
 
                 return View(contacts);
             }
+        }
 
+        [Authorize]
+        public ActionResult FriendRequest(int ProfileId, string requestButton)
+        {
+            // Current user
+            var userId = User.Identity.GetUserId();
+
+            using (var db = new DataContext())
+            {
+                var currentUser = db.Users.Single(c => c.Id.ToString() == userId);
+                var userProfile = currentUser.Profile;
+
+                var visitUserProfile = db.Profiles.Include(e => e.UserAccount).Where(p => p.Id == ProfileId).SingleOrDefault();
+
+                Contact contact = new Contact();
+                contact.User = userProfile;
+                contact.Request = true;
+                contact.Accept = false;
+                contact.Friend = visitUserProfile;
+
+                db.Contacts.Add(contact);
+                db.SaveChanges();
+
+                //var pwm = new ProfileViewModel();
+                //pwm.Profile = visitedProfile;
+                //pwm.PostIndexViewModel = new PostIndexViewModel();
+                //pwm.PostIndexViewModel.Id = currentUserProfile.Id;
+                //pwm.PostIndexViewModel.Posts = visitedUserProfiel.Posts;
+
+                //return View("PersonalUserSite", pwm);
+                return RedirectToAction("VisitProfile", "Account");
+            }   
         }
     }
 }
