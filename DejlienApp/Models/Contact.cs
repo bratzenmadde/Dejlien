baea941ddb42476c01937986cc0a0695add4f1d5
Linn Vretlund <32539427+Lvretlund@users.noreply.github.com>
2018-01-06 16:21:25 +0100

    almost there

--- DejlienApp/Models/Contact.cs
+++ DejlienApp/Models/Contact.cs
@@ -12,6 +12,7 @@ namespace DejlienApp.Models
         public int Id { get; set; }
         public  bool Request { get; set; }
         public bool Accept { get; set; }
+        public bool IAskedTheQuestion { get; set; }
 
         public virtual Profile User { get; set; }
         public virtual Profile Friend { get; set; }
