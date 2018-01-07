15c542b777671a94290662100cc16307c88df5a2
itsLoreena <frida.niklasson88@gmail.com>
2018-01-07 11:31:26 +0100

    Gjort search controller och flyttat vyn, gjort koden i partial vy snyggare

--- DejlienApp/Controllers/AccountController.cs
+++ DejlienApp/Controllers/AccountController.cs
@@ -216,35 +216,6 @@ namespace DejlienApp.Controllers
             return RedirectToAction("Index", "Home");
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize]
-        public ActionResult Search(string searchButton, string search)
-        {
-            if (String.IsNullOrEmpty(search))
-            {
-                return View();
-            }
-            else
-            {
-                using (var db = new DataContext())
-                {
-                    var SearchedProfiles = db.Profiles.Where(n => n.Name.Contains(search) && n.Visible == Visible.Yes);
-                    var SearchedP = SearchedProfiles.ToList();
-
-                    if (SearchedP == null)
-                    {
-                        return View();
-                    }
-
-                    else
-                    {
-                        return View(SearchedP);
-                    }
-                }
-            }
-        }
-
         [Authorize]
         public FileContentResult UserPhotos(int ProfileId)
         {
---
+++ DejlienApp/Controllers/SearchController.cs
@@ -0,0 +1,42 @@
+﻿using DejlienApp.Models;
+using DejlienApp.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DejlienApp.Controllers
+{
+    public class SearchController : Controller
+    {
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult Search(string searchButton, string search)
+        {
+            if (String.IsNullOrEmpty(search))
+            {
+                return View();
+            }
+            else
+            {
+                using (var db = new DataContext())
+                {
+                    var SearchedProfiles = db.Profiles.Where(n => n.Name.Contains(search) && n.Visible == Visible.Yes);
+                    var SearchedP = SearchedProfiles.ToList();
+
+                    if (SearchedP == null)
+                    {
+                        return View();
+                    }
+
+                    else
+                    {
+                        return View(SearchedP);
+                    }
+                }
+            }
+        }
+    }
+}
